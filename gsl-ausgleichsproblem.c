59ec38098b81f3db31b9b17e9447aa05944b7ff4
richardmac <fehlerrichard>
2019-01-14 18:35:00 +0100

    gsl version

--- gsl-ausgleichsproblem.c
+++ gsl-ausgleichsproblem.c
@@ -57,6 +57,11 @@ void readFile(char *name, double x[], double y[]) {
 
   free(line);
 }
+void printmatrix(gsl_matrix *matB, int N, int M) {
+  for (size_t i = 0; i < N; i++)
+    for (size_t j = 0; j < M; j++)
+      printf("m(%d,%d) = %g\n", i, j, gsl_matrix_get(matB, i, j));
+}
 // --------------------------------------------------------------------
 // --------------------------------------------------------------------
 
@@ -70,6 +75,7 @@ int main(int argc, char *argv[]) {
 
   double x[N];  // Vektor für den Abstand der Messung
   double y[N];  // Vektor für den gemessenen Wert
+  double matData[2 * N];
   //   Einlesen der Daten
   readFile("input.dat", x, y);
 
@@ -80,11 +86,49 @@ int main(int argc, char *argv[]) {
   gsl_vector_view f2_vector = gsl_vector_view_array(y, N);
   gsl_vector_fprintf(stdout, &f1_vector.vector, "%g");
 
+  // for (size_t i = 0; i < N; i++) {
+  //   f1_values[N] = f1(N);
+  //   f2_values[N] = f2(N);
+  // }
   for (size_t i = 0; i < N; i++) {
-    f1_values[N] = f1(N);
-    f2_values[N] = f2(N);
+    matData[i] = x[i];
+    matData[i + N] = y[i];
   }
+  /////////////////////////////////////
+  // gsl_matrix *m = gsl_matrix_alloc(10, 10);
 
+  // for (i = 0; i < 10; i++)
+  //   for (j = 0; j < 10; j++) gsl_matrix_set(m, i, j, sin(i) + cos(j));
+
+  // for (j = 0; j < 10; j++) {
+  //   gsl_vector_view column = gsl_matrix_column(m, j);
+  //   double d;
+
+  //   d = gsl_blas_dnrm2(&column.vector);
+
+  //   printf("matrix column %zu, norm = %g\n", j, d);
+  // }
+
+  // gsl_matrix_free(m);
+  /////////////////////
+  gsl_matrix *matB = gsl_matrix_alloc(N, 2);
+  gsl_matrix *matB_trans = gsl_matrix_alloc(2, N);
+  gsl_matrix *matC = gsl_matrix_alloc(2, 2);
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < 2; j++) {
+      gsl_matrix_set(matB, i, 0, matData[i]);
+      gsl_matrix_set(matB, i, 1, matData[N + i]);
+    }
+  }
+  // print the matrix
+  printmatrix(matB, N, 2);
+  gsl_matrix_transpose_memcpy(matB_trans, matB);
+
+  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, &matB_trans, &matB, 0.0,
+                 &matC);
+
+  gsl_matrix_view matA = gsl_matrix_view_array(matData, N, 2);
+  // gsl_matrix_fprintf(stdout, &matA.matrix, "%g");
   //_____________________________________________________________________
   // benötigte Variablen einlegen und initialisieren.
 

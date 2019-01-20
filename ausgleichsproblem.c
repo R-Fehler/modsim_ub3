efc1da80e8b7a9f27306351da87004c0e7b681ca
richardmac <fehlerrichard>
2019-01-20 18:59:00 +0100

    working but wrong answer

--- ausgleichsproblem.c
+++ ausgleichsproblem.c
@@ -1,5 +1,9 @@
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 /*********************************************************************/
 /*                                                                   */
 /* Es liegen Messdaten "input.dat" in Form von Wertepaaren vor.      */
@@ -59,7 +63,111 @@ void readFile(char *name, double x[], double y[]) {
 // --------------------------------------------------------------------
 
 // Definition der Ansatzfunktionen f1 und f2
+double f1(double x) { return (x / (1.0 + x)); }
+double f2(double x) { return (1.0 / (2.0 + x)); }
 
+void printmatrix(int r, int c, double mat[r][c]) {
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      printf("[%d,%d] %3f\t", i, j, mat[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void m_m_multiply(int m1_rows, int m1_cols, double m1[m1_rows][m1_cols],
+                  int m2_rows, int m2_cols, double m2[m2_rows][m2_cols],
+                  int r_r, int r_c, double result[r_r][r_c]) {
+  int m, n, p, q, c, d, k;
+  double sum = 0.0;
+  m = m1_rows;
+  n = m1_cols;
+  p = m2_rows;
+  q = m2_cols;
+
+  if (n != p)
+    printf("The matrices can't be multiplied with each other.\n");
+  else {
+    for (c = 0; c < m; c++) {
+      for (d = 0; d < q; d++) {
+        for (k = 0; k < p; k++) {
+          sum = sum + m1[c][k] * m2[k][d];
+        }
+        // printf("[%d,%d]summe:%f\n", c, d, sum);
+        result[c][d] = sum;
+        sum = 0;
+      }
+    }
+  }
+}
+
+void gsl_gauss(int m1_rows, int m1_cols, double m1[m1_rows][m1_cols], int r_r,
+               int r_c, double vector[r_r][r_c], double result[2]) {
+  // Declare pointer variables for two gsl vectors, a matrix,
+  // and a permutation list
+  gsl_vector *b, *x;
+  gsl_matrix *A;
+  gsl_permutation *p;
+  int s;
+  // Create the vector and the matrix
+  b = gsl_vector_alloc(r_c);
+  A = gsl_matrix_alloc(m1_rows, m1_cols);
+
+  // load them with values
+  for (int i = 0; i < r_c; i++) {
+    gsl_vector_set(b, i, vector[i][r_c]);
+  }
+
+  for (int i = 0; i < m1_rows; i++) {
+    for (int j = 0; j < m1_cols; j++) {
+      gsl_matrix_set(A, i, j, m1[i][j]);
+    }
+  }
+
+  // Solve the system A x = b
+  // Start by creating a vector to receive the result
+  // and a permutation list to pass to the LU decomposition
+  x = gsl_vector_alloc(r_c);
+  p = gsl_permutation_alloc(r_c);
+
+  // Do the LU decomposition on A and use it to solve the system
+  gsl_linalg_LU_decomp(A, p, &s);
+  gsl_linalg_LU_solve(A, p, b, x);
+
+  // Print the result
+  printf("The solution:\n");
+
+  gsl_vector_fprintf(stdout, x, "%f");
+  for (int i = 0; i < r_c; i++) {
+    result[i] = gsl_vector_get(x, i);
+  }
+  // Clean up
+  gsl_vector_free(b);
+  gsl_vector_free(x);
+  gsl_permutation_free(p);
+  gsl_matrix_free(A);
+}
+
+void transpose(int m1_rows, int m1_cols, double m1[m1_rows][m1_cols], int r_r,
+               int r_c, double result[r_r][r_c]) {
+  {
+    int m, n, c, d;
+
+    m = m1_rows;
+    n = m1_cols;
+    c = r_r;
+    d = r_c;
+
+    for (c = 0; c < m; c++)
+      for (d = 0; d < n; d++) result[d][c] = m1[c][d];
+
+    // printf("Transpose of the matrix:\n");
+
+    // for (c = 0; c < n; c++) {
+    //   for (d = 0; d < m; d++) printf("%d\t", m1[c][d]);
+    //   printf("\n");
+  }
+}
 int main(int argc, char *argv[]) {
   // Abzählen der Wertepaare
   int N = getNumberofPoints("input.dat");
@@ -69,8 +177,37 @@ int main(int argc, char *argv[]) {
   //   Einlesen der Daten
   readFile("input.dat", x, y);
 
-  double lambda1 = 0.0;  // Koeffizient für die Funktion f1
-  double lambda2 = 0.0;  // Koeffizient für die Funktion f2
+  double lambda[2] = {0.0, 0.0};  // Koeffizient für die Funktion f1 , f2
+  double mat_A[N][2];
+  double mat_A_tr[2][N];
+  double mat_A_t_A[2][2];
+  double vector[1][2];
+  double yy[N][1];
+
+  for (int i = 0; i < N; i++) {
+    mat_A[i][0] = f2(x[i]);
+    mat_A[i][1] = f1(x[i]);
+  }
+  for (int i = 0; i < N; i++) {
+    yy[i][1] = x[i];
+  }
+  printf("MATRIX A\n");
+  printmatrix(N, 2, mat_A);
+  transpose(N, 2, mat_A, 2, N, mat_A_tr);
+  printf("matrix Atr\n");
+  printmatrix(2, N, mat_A_tr);
+  printf("matrix Atr vollständig\n");
+  m_m_multiply(2, N, mat_A_tr, N, 2, mat_A, 2, 2, mat_A_t_A);
+  printf("MATRIX Atr x A \n");
+  printmatrix(2, 2, mat_A_t_A);
+  printf("matrix a_tr x y\n");
+  m_m_multiply(2, N, mat_A_tr, N, 1, yy, 1, 2, vector);
+  printmatrix(1, 2, vector);
+  gsl_gauss(2, 2, mat_A_t_A, 1, 2, vector, lambda);
+
+  for (int i = 0; i < 2; i++) {
+    printf("lambda %d : %f \n", i, lambda[i]);
+  }
 
   //_____________________________________________________________________
   // benötigte Variablen einlegen und initialisieren.
@@ -78,20 +215,21 @@ int main(int argc, char *argv[]) {
   //   Berechnung von lambda1
 
   // Plotten wenn plotflag!=0
-  long plotflag = 0;
+  long plotflag = 1;
 
   if (plotflag) {
     char plotbefehl[10000];
     sprintf(plotbefehl,
             " echo "
             " ' reset; set key right top Left box; \n"
+            "set term jpeg;\n"
+            "set output plot.jpg;\n"
             " set xrange [0.0:100]; set xlabel \"x\"; \n"
             " set yrange [0.0:0.75]; set ylabel \"y\"; \n"
             " f(x) = %le*x*(1+x)**(-1)+%le*(2+x)**(-1);\n "
             " plot f(x) lt -1 lw 2, \"input.dat\" u 1:2 pt 7;\n"
-            " set term eps; ' "
-            " | gnuplot -persist",
-            lambda1, lambda2);
+            " ' | gnuplot -persist>>test.jpg",
+            lambda[0], lambda[1]);
 
     system(plotbefehl);
   }
